2cf012c1494237c1be3cc0b8949f87afc2dd9d5b
yager-j <joanneyager@gmail.com>
2024-02-23 14:03:34 +0100

    Initial Commit

---
+++ BikeRadar/BikeRadarApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikeRadarApp.swift
+//  BikeRadar
+//
+//  Created by Joanne Yager on 2024-02-23.
+//
+
+import SwiftUI
+
+@main
+struct BikeRadarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
