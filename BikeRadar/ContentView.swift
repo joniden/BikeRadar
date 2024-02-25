a3446722aa15fca6c93d5971bb21f78f864b0fb6
yager-j <joanneyager@gmail.com>
2024-02-25 20:48:24 +0100

    Show a list of cities then show a list of networks in the selected city

--- BikeRadar/ContentView.swift
+++ BikeRadar/ContentView.swift
@@ -6,32 +6,68 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
     
     @StateObject private var dataService = LocationsDataService()
-    @State private var city: String = ""
+    @State private var textInput: String = ""
+    let textInputPublisher = PassthroughSubject<String, Never>()
+    @State private var searchText = ""
+    @State private var selectedCity: String? = nil
+    @State private var networks: [Network] = []
     
     var body: some View {
         NavigationStack {
             VStack {
-                TextField("Search for your city", text: $city)
+                TextField("Search for your city", text: $textInput)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-                Text("You entered: \(city)")
-                
-                VStack {
+                    .onChange(of: textInput) {
+                        textInputPublisher.send(textInput)
+                    }
+                    .onReceive(textInputPublisher
+                        .debounce(for: .milliseconds(300), scheduler: DispatchQueue.main)
+                        .removeDuplicates()
+                    ) { debouncedTextInput in
+                        searchText = debouncedTextInput
+                    }
+                ScrollView {
                     if dataService.networks.isEmpty {
                         Text("Loading data...")
-                        Spacer()
                     } else {
-                        List(dataService.networks) { network in
-                            if city == network.location?.city {
-                                NavigationLink(destination: MapView(dataService: dataService, network: network)) {
-                                    Text(network.name ?? "No network name")
+                        if let selectedCity {
+                            // Show list of networks for selected city
+                            if networks.count > 0 {
+                                ForEach(networks) { network in
+                                   
+                                    NavigationLink(destination: MapView(dataService: dataService, network: network)) {
+                                        Text(network.name ?? "test")
+                                    }
                                 }
                             }
+                            Spacer()
+                        } else {
+                            // Show list of cities
+                            LazyVStack {
+                                ForEach(filteredCities, id: \.self) { city in
+                                    Button {
+                                        selectedCity = city
+                                    } label: {
+                                        Text(city)
+                                    }
+                                }
+                                Spacer()
+                            }
                         }
+                    }
+                }
+                .padding()
+                .onChange(of: selectedCity) {
+                    if let selectedCity {
+                        networks = networksForCity(selectedCity)
+                    } else {
+                        networks = []
                     }
                 }
             }
@@ -40,11 +76,28 @@ struct ContentView: View {
             do {
                 try await dataService.fetchData()
                 print("networks: \(dataService.networks.count)")
-                print("networks id: \(dataService.networks[0].id)")
             } catch {
                 // handle error
                 print("Failed to fetch data: \(error)")
             }
+        }
+        .onChange(of: selectedCity) {
+            print("selectedCity \(selectedCity)")
+        }
+    }
+    
+    private var filteredCities: [String] {
+        return dataService.networks
+            .compactMap { $0.location?.city }
+            .filter {
+                $0.lowercased().contains(searchText.lowercased())
+            }
+    }
+    
+    private func networksForCity(_ city: String) -> [Network] {
+        print("Fetching networks for \(city)")
+        return dataService.networks.filter { network in
+            return network.location?.city.lowercased() == city.lowercased()
         }
     }
 }
@@ -52,3 +105,34 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+/*struct FancyLoadingView: View {
+    @State private var rotationAngle: Double = 0.0
+    
+    private var animation: Animation {
+        .linear
+        .speed(0.1)
+        .repeatForever(autoreverses: false)
+    }
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "bicycle")
+                .rotationEffect(Angle(degrees: rotationAngle))
+                .foregroundColor(.blue)
+                .font(.system(size: 50))
+            
+            Text("Loading data...")
+                .font(.headline)
+                .foregroundColor(.gray)
+                .opacity(0.5)
+                .padding(.top, 8)
+        }
+        .onAppear {
+            withAnimation(animation) {
+                rotationAngle += 360.0
+            }
+        }
+    }
+}*/
+
